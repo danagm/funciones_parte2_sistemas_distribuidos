af5f6874018add392cb40372ea284da219dd56b8
danagm <43969499+danagm@users.noreply.github.com>
2020-10-17 22:51:28 -0500

    código de impares

---
+++ impares.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func generadorImpares() func() uint {
+	i := uint(1) // i permanecerá en el clousure de la función anónima a retornar
+	return func() uint {
+		var impar = i
+		i += 2
+		return impar
+	}
+}
+
+func main() {
+	nextImpar := generadorImpares()
+	fmt.Println(nextImpar())
+	fmt.Println(nextImpar())
+	fmt.Println(nextImpar())
+}
